34103b670b892216a7abba3f0a7c6522b13ceb08
George Fofiu <fofiu.george@yahoo.com>
2025-06-24 16:38:19 +0300

    added aos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -12,7 +14,9 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
-    MatIconModule,],
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -23,5 +27,32 @@ export class HomeComponent {
     { icon: 'fas fa-crosshairs',  title: 'Sedare Perspiciatis' },
     { icon: 'fas fa-list',        title: 'Magni Dolores' },
     { icon: 'fas fa-gem',         title: 'Nemos Enimade' },
+  ];
+
+  features = [
+    {
+      img: 'big-image.jpg',
+      matIcon: 'inventory_2',
+      title: 'Management complet',
+      text: 'Management complet al proprietății tale, de la A la Z'
+    },
+    {
+      img: 'big-image.jpg',
+      matIcon: 'bar_chart',
+      title: 'Optimizare costuri',
+      text: 'Optimizați costurile și resursele, fără compromisuri'
+    },
+    {
+      img: 'big-image.jpg',
+      matIcon: 'rocket_launch',
+      title: 'Intervenții rapide',
+      text: 'Intervenții rapide, eficiență și profesionalism constant'
+    },
+    {
+      img: 'big-image.jpg',
+      matIcon: 'lock',
+      title: 'Siguranță & confort',
+      text: 'Siguranță și confort pentru tine și chiriași'
+    }
   ];
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { provideRouter }        from '@angular/router';
 import { AppComponent }         from './app/app.component';
 import { appRoutes }            from './app/app.routes';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({ once: true, duration: 600 });
+
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
