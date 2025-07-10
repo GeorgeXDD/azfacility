2b314a5404e3df7377346d3f140dcb6e86c0fda3
George Fofiu <fofiu.george@yahoo.com>
2025-07-10 15:35:35 +0300

    added improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule }           from '@angular/material/toolbar';
 import { MatMenuModule }              from '@angular/material/menu';
 import { MatButtonModule }            from '@angular/material/button';
 import { MatIconModule }                from '@angular/material/icon'; 
 import { CommonModule }               from '@angular/common';
-import { RouterModule }               from '@angular/router';
+import { NavigationEnd, RouterModule, Router }       from '@angular/router';
 import { MatSidenavModule }           from '@angular/material/sidenav';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -23,5 +24,19 @@ import { MatSidenavModule }           from '@angular/material/sidenav';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AZFacility'
+  title = 'AZFacility';
+  currentUrl: string = '';
+  private router = inject(Router); // ✅ fix: inject Router manually
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentUrl = (event as NavigationEnd).urlAfterRedirects;
+    });
+  }
+
+  isServiciiActive(): boolean {
+    return this.currentUrl.startsWith('/servicii');
+  }
 }
