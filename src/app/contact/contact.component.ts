65fd0f31d26fb00a0a72a8a80e512f71822ffdf5
Razvan Calinescu <rcalinescu@valeo-it.com>
2025-07-18 13:05:44 +0300

    Contact basic done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+    contactForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      service: ['', Validators.required],
+      message: ['', Validators.required],
+      terms: [false, [Validators.requiredTrue]] // custom validator for checkbox
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // TODO: trimite către backend
+    }
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.contactForm.controls;
+  }
 }
