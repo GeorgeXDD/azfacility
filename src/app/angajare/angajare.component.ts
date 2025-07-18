3c97c56ca175d7561990f815e1147ba3ae869648
George Fofiu <fofiu.george@yahoo.com>
2025-07-18 12:49:14 +0300

    pagina de cariere functionala

--- src/app/angajare/angajare.component.ts
+++ src/app/angajare/angajare.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-angajare',
-  imports: [],
+    imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+  ],
   templateUrl: './angajare.component.html',
   styleUrl: './angajare.component.scss'
 })
 export class AngajareComponent {
+  availablePositions = [
+    { title: 'Tehnicieni mentenanță', description: '(electricieni, instalatori, HVAC)' },
+    { title: 'Operatori de curățenie', description: 'Spații comerciale/rezidențiale' },
+    { title: 'Agenti de vânzări servicii facility management', description: 'descriere' },
+    { title: 'Personal pentru pază și supraveghere', description: 'descriere' },
+    { title: 'Agent întreținere spații verzi', description: 'descriere' },
+    { title: 'Agent de vânzări servicii & management', description: 'descriere' },
+    { title: 'Specialist marketing & promovare', description: 'descriere' },
+    { title: 'Part Time worker', description: 'descriere' },
+  ];
 
+  firstColumn: typeof this.availablePositions = [];
+  secondColumn: typeof this.availablePositions = [];
+
+  ngOnInit(): void {
+    const middleIndex = Math.ceil(this.availablePositions.length / 2);
+    this.firstColumn = this.availablePositions.slice(0, middleIndex);
+    this.secondColumn = this.availablePositions.slice(middleIndex);
+  }
 }
